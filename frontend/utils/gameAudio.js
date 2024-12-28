53e2ee3d9b697cb8aaa4250136fe5899c182ef0e
Aytsuu <paoloaraneta008@gmail.com>
2024-12-28 12:11:52 +0800

    update folder

--- frontend/utils/gameAudio.js
+++ frontend/utils/gameAudio.js
@@ -1,47 +1,47 @@
 //---------------------- MENU SHIFT AUDIO ----------------------------//
 
-const menuClickAudio = new Audio('/frontend/sounds/main menu/click.wav');
+const menuClickAudio = new Audio('/frontend/assets/sounds/main menu/click.wav');
 
 
 //---------------------- MAZE SHIFT AUDIO ----------------------------//
 
-const mazeShiftAudio = new Audio('/frontend/sounds/maze/shift2.wav');
+const mazeShiftAudio = new Audio('/frontend/assets/sounds/maze/shift2.wav');
 mazeShiftAudio.volume = 0.6;
 
 
 //---------------------- PLAYER AUDIO ----------------------------//
 
-const playerRunAudio = new Audio('/frontend/sounds/player/running.wav');
+const playerRunAudio = new Audio('/frontend/assets/sounds/player/running.wav');
 playerRunAudio.volume = 0.6;
 playerRunAudio.currentTime = 0.02;
 
-const playerAttackAudio = new Audio('/frontend/sounds/player/attack.wav');
+const playerAttackAudio = new Audio('/frontend/assets/sounds/player/attack.wav');
 playerAttackAudio.volume = 0.5;
 
-const parryAudio = new Audio('/frontend/sounds/player/parry3.wav');
+const parryAudio = new Audio('/frontend/assets/sounds/player/parry3.wav');
 parryAudio.volume = 0.6;
 
 
 //---------------------- SHADOW AUDIO ----------------------------//
 
-const shadowAttackAudio = new Audio('/frontend/sounds/ghost/Ghost_Attack.wav');
+const shadowAttackAudio = new Audio('/frontend/assets/sounds/ghost/Ghost_Attack.wav');
 shadowAttackAudio.volume = 0.6;
 shadowAttackAudio.currentTime = 0.015;
 
-const shadowFazed = new Audio('/frontend/sounds/ghost/Ghost_Damage.wav');
+const shadowFazed = new Audio('/frontend/assets/sounds/ghost/Ghost_Damage.wav');
 shadowFazed.volume = 0.6;
 
-const shadowWarpAudio = new Audio('/frontend/sounds/ghost/Ghost_Emerge.wav');
+const shadowWarpAudio = new Audio('/frontend/assets/sounds/ghost/Ghost_Emerge.wav');
 shadowWarpAudio.volume = 0.6;
 
 //---------------------- SHADE AUDIO ----------------------------//
 
-const shadeAttackAudio = new Audio('/frontend/sounds/redenemy/redattack.wav');
+const shadeAttackAudio = new Audio('/frontend/assets/sounds/redenemy/redattack.wav');
 shadeAttackAudio.volume = 0.6;
 shadeAttackAudio.currentTime = 0.015;
 
-const shadeFazed = new Audio('/frontend/sounds/redenemy/reddamage.wav');
+const shadeFazed = new Audio('/frontend/assets/sounds/redenemy/reddamage.wav');
 shadeFazed.volume = 0.7;
 
-const shadeWarpAudio = new Audio('/frontend/sounds/redenemy/redemerge.wav');
+const shadeWarpAudio = new Audio('/frontend/assets/sounds/redenemy/redemerge.wav');
 shadeWarpAudio.volume = 0.7;
