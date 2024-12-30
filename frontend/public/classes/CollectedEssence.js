c1f085d24cded63ead8e4c9d626fffc7c3f7a104
Aytsuu <paoloaraneta008@gmail.com>
2024-12-30 22:01:02 +0800

    for server

--- frontend/public/classes/CollectedEssence.js
+++
@@ -1,9 +0,0 @@
-class CollectedEssence extends Sprite{
-    constructor({ imgSrc, frameRate, imgSize, position, animations }){
-        super({ imgSrc, frameRate, animations });
-
-        this.imgSize = imgSize;
-        this.position = position;
-
-    }
-}
